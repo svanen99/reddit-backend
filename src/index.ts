6b3ceb3faac3aa964109b7c00c1d26b6e06c3490
Lisette Svan <svanlisette@gmail.com>
2025-06-16 16:06:42 +0200

    edit

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,6 @@ app.get("/", (req, res) => {
 });
 
 mongoose.connect(process.env.DB_URL!).then(() => {
-  const PORT = process.env.PORT;
+  const PORT = process.env.PORT || '8080'
   app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 });
